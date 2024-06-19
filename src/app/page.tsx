ed52043e22bd83a1c862401595d6af76ebc26572
jbilee <julishahn@gmail.com>
2024-06-19 16:49:49 +0900

    fix build time error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,13 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useState, type FormEvent } from "react";
-import { Layer, Stage } from "react-konva";
-import Bubble from "~/components/Bubble";
-
-type BubbleProps = {
-  text: string;
-  id: string;
-  x: number;
-  y: number;
-};
-
-const getRandomValue = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max + 1 - min) + min);
-};
+const KonvaCanvas = dynamic(() => import("~/components/Canvas"), {
+  ssr: false,
+});
 
 export default function HomePage() {
-  const [input, setInput] = useState("");
-  const [bubbles, setBubbles] = useState<BubbleProps[]>([]);
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setBubbles((prev) => [
-      ...prev,
-      {
-        text: input,
-        id: crypto.randomUUID(),
-        x: getRandomValue(0, 900),
-        y: getRandomValue(0, 500),
-      },
-    ]);
-    setInput("");
-  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="border border-black">
-        <Stage width={1200} height={600}>
-          <Layer>
-            {bubbles.map((elem) => (
-              <Bubble key={elem.id} text={elem.text} x={elem.x} y={elem.y} />
-            ))}
-            {/* <Rect
-              cornerRadius={10}
-              x={500}
-              y={100}
-              width={100}
-              height={50}
-              fill="coral"
-              draggable
-            />
-            <Circle x={200} y={100} radius={50} fill="green" draggable />
-            <Text text="hello????l" fontSize={20} draggable /> */}
-          </Layer>
-        </Stage>
-      </div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <input type="submit" value="Add" />
-      </form>
+      <KonvaCanvas />
     </main>
   );
 }
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Layer, Stage } from "react-konva";
+import Bubble from "./Bubble";
+import { type FormEvent, useState } from "react";
+
+type BubbleProps = {
+  text: string;
+  id: string;
+  x: number;
+  y: number;
+};
+
+const getRandomValue = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+export default function KonvaCanvas() {
+  const [input, setInput] = useState("");
+  const [bubbles, setBubbles] = useState<BubbleProps[]>([]);
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setBubbles((prev) => [
+      ...prev,
+      {
+        text: input,
+        id: crypto.randomUUID(),
+        x: getRandomValue(0, 900),
+        y: getRandomValue(0, 500),
+      },
+    ]);
+    setInput("");
+  };
+  return (
+    <>
+      <div className="border border-black">
+        <Stage width={1200} height={600}>
+          <Layer>
+            {bubbles.map((elem) => (
+              <Bubble key={elem.id} text={elem.text} x={elem.x} y={elem.y} />
+            ))}
+            {/* <Rect
+        cornerRadius={10}
+        x={500}
+        y={100}
+        width={100}
+        height={50}
+        fill="coral"
+        draggable
+      />
+      <Circle x={200} y={100} radius={50} fill="green" draggable />
+      <Text text="hello????l" fontSize={20} draggable /> */}
+          </Layer>
+        </Stage>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input type="submit" value="Add" />
+      </form>
+    </>
+  );
+}
